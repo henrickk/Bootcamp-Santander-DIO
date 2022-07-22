9412d32be84f557e78716679790b3a3fe799f8f2
Henrickk <henrick.adrian4@gmail.com>
2022-07-21 23:58:09 -0300

    Introdução a Javascript

---
+++ Javascript/teste.js
@@ -0,0 +1,11 @@
+// diferença de variável e constante
+
+//Constantes não podem ter o valor alterado
+
+//var preco = 2
+
+var desconto = 0.2
+
+const PRECO = 2 //Boa prática usar o nome da constante em maiúscolo
+
+var total = PRECO - desconto
