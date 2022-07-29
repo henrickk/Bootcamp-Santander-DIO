2c44cfef531806a12185f73b0808d12733a21de2
Henrickk <henrick.adrian4@gmail.com>
2022-07-28 23:12:46 -0300

    Atividade filter

---
+++ Javascript/DIO/atividades/atividadeFilter.js
@@ -0,0 +1,12 @@
+function filtrePares(arr) {
+  return arr.filter(callback)
+}
+
+function callback(item) {
+  return item % 2 === 0
+}
+
+const meuArray = [1, 23, 55, 67, 30, 2, 4]
+
+console.log(filtrePares(meuArray))
+
