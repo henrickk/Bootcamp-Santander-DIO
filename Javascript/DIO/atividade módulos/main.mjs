8375a0f319b8b9da860237efe225c9f9e96afb65
Henrickk <henrick.adrian4@gmail.com>
2022-08-03 20:39:20 -0300

    Atividade módulos

---
+++ Javascript/DIO/atividade módulos/main.mjs
@@ -0,0 +1,5 @@
+import {mostraIdade, mostraHobby, mostraCidade } from "./funcoes.mjs";
+
+console.log(mostraIdade("Henrick", 21))
+console.log(mostraCidade("Henrick", "São Paulo"))
+console.log(mostraHobby("Henrick", "Andar de skate "))
